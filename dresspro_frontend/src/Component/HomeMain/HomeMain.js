3e075986d47e4d248d02f014fba4bc764eb5539b
baniex <banjibaniex1@gmail.com>
2023-01-02 01:46:50 +0100

    completed home page

--- dresspro_frontend/src/Component/HomeMain/HomeMain.js
+++ dresspro_frontend/src/Component/HomeMain/HomeMain.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./HomeMain.css"
+import { Link } from "react-router-dom";
+
 // import plane from "../../assets/images/plane.png"
 // import container from "../../assets/images/container.png";
 
@@ -7,9 +9,10 @@ import img from "../../assets/images/cargo6.jpg";
 
 
 
-const HomeMain = () => {
+const HomeMain = (props) => {
+  let navbar_closer = props.navbar_closer;
   return (
-    <div>
+    <div onClick={navbar_closer}>
       {/* classname below should be read as home_first_level depicting the layer of the div */}
       <div className="home_first">
         <div className="home_image"></div>
@@ -23,7 +26,9 @@ const HomeMain = () => {
           best possible way, creating long term business relationship.
         </div>
         <div className="home_second_button">
-          <div className="home_second_button_subdiv">Contact Us</div>
+          <div className="home_second_button_subdiv">
+            <Link to="/contact">Contact Us</Link>
+          </div>
         </div>
       </div>
       <div className="home_third">
@@ -43,9 +48,7 @@ const HomeMain = () => {
                   </div>
                   <div className="home_third_first_content_secondary">
                     Fastest shipping method of goods, via air carrier and safe
-                    delivery. Shipping at the speed of light Fastest shipping
-                    method of goods, via air carrier and safe delivery_Shipping
-                    at the speed of light
+                    delivery.
                   </div>
                 </div>
               </div>
@@ -59,13 +62,10 @@ const HomeMain = () => {
               <div className="home_third_first_content">
                 <div className="home_third_first_content_subdiv">
                   <div className="home_third_first_content_primary">
-                    Air shipment
+                    Proper inspection
                   </div>
                   <div className="home_third_first_content_secondary">
-                    Fastest shipping method of goods, via air carrier and safe
-                    delivery. Shipping at the speed of light Fastest shipping
-                    method of goods, via air carrier and safe delivery_Shipping
-                    at the speed of light
+                    Proper inspection of goods against contraband brfore transit to ensure 100% success rate in delivery
                   </div>
                 </div>
               </div>
@@ -79,13 +79,11 @@ const HomeMain = () => {
               <div className="home_third_first_content">
                 <div className="home_third_first_content_subdiv">
                   <div className="home_third_first_content_primary">
-                    Air shipment
+                    Documentation
                   </div>
                   <div className="home_third_first_content_secondary">
-                    Fastest shipping method of goods, via air carrier and safe
-                    delivery. Shipping at the speed of light Fastest shipping
-                    method of goods, via air carrier and safe delivery_Shipping
-                    at the speed of light
+                    Properly documented custom paper and legal works for maximum
+                    protection of goods in transit and at border
                   </div>
                 </div>
               </div>
@@ -93,7 +91,28 @@ const HomeMain = () => {
           </div>
         </div>
       </div>
-      <div className="home_fourth"></div>
+      <div className="home_fourth">
+        <div className="home_numbers_one">
+          <div className="home_numbers_up">132000 +</div>
+          <div className="home_numbers_middle"></div>
+          <div className="home_numbers_down">Packages Delivered</div>
+        </div>
+        <div className="home_numbers_one">
+          <div className="home_numbers_up">15+</div>
+          <div className="home_numbers_middle"></div>
+          <div className="home_numbers_down">Destination Countries</div>
+        </div>
+        <div className="home_numbers_one">
+          <div className="home_numbers_up">10000 +</div>
+          <div className="home_numbers_middle"></div>
+          <div className="home_numbers_down">Customers</div>
+        </div>
+        <div className="home_numbers_one">
+          <div className="home_numbers_up">95%</div>
+          <div className="home_numbers_middle"></div>
+          <div className="home_numbers_down">Customer Satisfaction Rate</div>
+        </div>
+      </div>
       <div className="home_fifth"></div>
     </div>
   );
